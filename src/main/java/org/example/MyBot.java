fb0f5a0d6f01180f7c3481cb5f769581091fcdca
Abduazizxon <turayevabduazizxon@gmail.com>
2024-02-14 22:47:38 +0500

    BotButtonService va MyBot fayllarda yangilansh

--- src/main/java/org/example/MyBot.java
+++ src/main/java/org/example/MyBot.java
@@ -3,15 +3,19 @@ package org.example;
 import org.example.States.BotState;
 import org.example.States.Roles;
 import org.example.model.User;
+import org.example.service.BotButtonService;
 import org.example.service.impl.UserServiceImpl;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.IOException;
 
 public class MyBot extends TelegramLongPollingBot {
     UserServiceImpl userService = new UserServiceImpl();
+    BotButtonService buttonService = new BotButtonService();
     public MyBot(String botToken){
         super(botToken);
     }
@@ -27,13 +31,28 @@ public class MyBot extends TelegramLongPollingBot {
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
+                SendMessage message = new SendMessage();
+                message.setChatId(chatId);
+                message.setText("Assalomu aleykum. Tilni tanlang\n\n" +
+                        "Привет. Выберите язык");
+                message.setReplyMarkup(buttonService.language());
+                try {
+                    execute(message);
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
             }else {
                 User user = userService.get(chatId);
                 switch (user.getState()){
                     case LANG -> {
-                        if (text.equals("O'zbek tili")){
-
-                        } else if (text.equals("Rus tili")) {
+                        if (text.equals("\uD83C\uDDFA\uD83C\uDDFF O'zbek tili")){
+                            user.setState(BotState.REGISTRATION);
+                            EditMessageText txt = new EditMessageText();
+                            txt.setMessageId(1);
+                            txt.setText("Yaxshi endi registratsiyani davom ettirish uchun botga qanday maqsadda kirganingizni yozing");
+                            txt.setChatId(chatId);
+                            txt.setReplyMarkup(buttonService.sales());
+                        } else if (text.equals("Русский язык \uD83C\uDDF7\uD83C\uDDFA")) {
 
                         }
                     }case MAIN -> {
@@ -41,6 +60,8 @@ public class MyBot extends TelegramLongPollingBot {
                     }
                 }
             }
+        } else if (update.hasCallbackQuery()) {
+
         }
     }
 
--- src/main/java/org/example/service/BotButtonService.java
+++ src/main/java/org/example/service/BotButtonService.java
@@ -1,6 +1,8 @@
 package org.example.service;
 
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
@@ -14,11 +16,11 @@ public class BotButtonService {
         KeyboardRow row = new KeyboardRow();
 
         KeyboardButton button1 = new KeyboardButton();
-        button1.setText("O'zbek tili");
+        button1.setText("\uD83C\uDDFA\uD83C\uDDFF O'zbek tili");
         row.add(button1);
 
         KeyboardButton button2 = new KeyboardButton();
-        button2.setText("Rus Tili");
+        button2.setText("Русский язык \uD83C\uDDF7\uD83C\uDDFA");
         row.add(button2);
 
         rowList.add(row);
@@ -26,6 +28,26 @@ public class BotButtonService {
         markup.setResizeKeyboard(true);
         markup.setOneTimeKeyboard(true);
         markup.setSelective(true);
+
+        return markup;
+    }
+    public InlineKeyboardMarkup sales(){
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setCallbackData("Sotuvchi mijoz");
+        button1.setText("Sotuvchi");
+        row.add(button1);
+
+        InlineKeyboardButton button2 = new InlineKeyboardButton();
+        button2.setText("Sotib oluvchi");
+        button2.setCallbackData("Mijoz");
+        row.add(button2);
+
+        rowList.add(row);
+        markup.setKeyboard(rowList);
 
         return markup;
     }
